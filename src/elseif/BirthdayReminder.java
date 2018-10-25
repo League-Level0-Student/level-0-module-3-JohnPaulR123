4dd1f0f3877513615ecf5a136d10e7f405f4c437
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-25 13:25:10 -0700

    Years alive, Birthday Reminder, Shiny Objects

--- src/elseif/BirthdayReminder.java
+++ src/elseif/BirthdayReminder.java
@@ -9,23 +9,35 @@ import javax.swing.JOptionPane;
 public class BirthdayReminder {
 
 	public static void main(String[] args) {
-	
+
 		// 1. correct the birthdays for your family below
-		String momsBirthday = "January 24th";
-		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
+		String momsBirthday = "January 1st";
+		String dadsBirthday = "August 17th";
+		String myBirthday = "May 15th";
 
-		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+		// 2. Find out which birthday the user wants and and store their response in a
+		// variable
+		String wants = JOptionPane.showInputDialog("Who's birthday would you like to know.");
 		// 3. Print out what the user typed
-		
+		System.out.println(wants);
 		// 4. if user asked for "mom"
-			//print mom's birthday
+		// print mom's birthday
+		if (wants.equalsIgnoreCase("mom")) {
+			JOptionPane.showMessageDialog(null, momsBirthday);
+		}
 		// 5. if user asked for "dad"
-			// print dad's birthday
+		// print dad's birthday
+		else if (wants.equalsIgnoreCase("dad")) {
+			JOptionPane.showMessageDialog(null, dadsBirthday);
+		}
 		// 6. if user asked for your name
-			// print myBirthday
-		//7. otherwise print "Sorry, i don't remember that person's birthday!"
-
-	} 
+		// print myBirthday
+		else if (wants.equalsIgnoreCase("John Paul")) {
+			JOptionPane.showMessageDialog(null, myBirthday);
+		}
+		// 7. otherwise print "Sorry, i don't remember that person's birthday!"
+		else {
+			JOptionPane.showMessageDialog(null, "Sorry, I don't remember that person's birthday!");
+		}
+	}
 }
--- src/loops/ShinyObjects.java
+++ src/loops/ShinyObjects.java
@@ -5,19 +5,22 @@ package loops;
 
 import java.applet.AudioClip;
 
+import javax.print.attribute.IntegerSyntax;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JApplet;
-
+import javax.swing.JOptionPane;
 
 public class ShinyObjects {
 	public static void main(String[] args) {
 		// 2. Ask the user how many shiny objects they want
-
+		String shiny = JOptionPane.showInputDialog("How many shiny objects would you like?");
 		// 3. Play the sound that many times
+		for (int i = 0; i < Integer.parseInt(shiny); i++) {
 
-		// 1. Call the method below
-
+			// 1. Call the method below
+			playMisterZee();
+		}
 	}
 
 	public static void playMisterZee() {
@@ -29,6 +32,5 @@ public class ShinyObjects {
 			ex.printStackTrace();
 		}
 	}
-
 
 }
