57a244f6044f4c73344c67d4cbbabcdbcb66177b
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-11-01 13:25:43 -0700

    Walk of Fame

--- src/loops/WalkOfFame.java
+++ src/loops/WalkOfFame.java
@@ -11,9 +11,20 @@ public class WalkOfFame {
 		Robot rob = new Robot();
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
-
+		rob.setX(150);
+		rob.setSpeed(50);
 		// 2. Make the robot draw a star shape. Hint: 144.
-
+		for(int m=0;m<10;m++) {
+		for(int i=0;i<5;i++) {
+		rob.penDown();
+		rob.move(30);
+		rob.turn(144);
+		}
+		rob.penUp();
+		rob.turn(90);
+		rob.move(50);
+		rob.turn(-90);
+		}
 		// 3. Set the size of the star to 30.
 
 		/** THE CHALLENGE: **/
